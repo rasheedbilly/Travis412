b1959253453897b6e0f23a5f4f5fb0064b1fc3ff
rasheedbilly <rasheedbilly@gmail.com>
2021-02-21 19:56:23 -0500

    added Divide

--- src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
+++ src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
@@ -12,4 +12,10 @@ public class SimpleCalculator {
 	public int multiply(int a, int b) {
 		return a * b;
 	}
+
+	public int divide(int a, int b) throws Exception{
+		if(b == 0)
+			throw new Exception("Exception message");
+		return a / b;
+	}
 }
--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -21,10 +21,15 @@ public class SimpleCalculatorTest {
 		assertEquals(calc.minus(5, 3), 2);
 	}
 	
-	
 	@Test
 	public void testMultiply() {
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.multiply(5, 3), 15);
+	}
+	
+	@Test
+	public void testDivide() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.divide(6, 3), 2);
 	}
 }
